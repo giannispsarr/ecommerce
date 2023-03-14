719efbe8b1921da24eb0d55d7c03d1ef066e10e1
Giannis Psarras <giannispsarr@gmail.com>
2023-03-14 19:23:48 -0400

    Footer and LandingScreen

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import CartScreen from './screens/CartScreen';
+import LandingScreen from './screens/LandingScreen';
 import ProductScreen from './screens/ProductScreen';
 import ProductsScreen from './screens/ProductsScreen';
 
@@ -12,11 +14,13 @@ function App() {
         <Navbar />
         <main>
           <Routes>
+            <Route path='/' element={<LandingScreen />} />
             <Route path='/products' element={<ProductsScreen />} />
             <Route path='/product/:id' element={<ProductScreen />} />
             <Route path='/cart' element={<CartScreen />} />
           </Routes>
         </main>
+        <Footer />
       </Router>
     </ChakraProvider>
   );
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -15,6 +15,8 @@ import {
 import { Link as ReactLink } from 'react-router-dom';
 import { HamburgerIcon, CloseIcon, MoonIcon, SunIcon } from '@chakra-ui/icons';
 import { GiTechnoHeart } from 'react-icons/gi';
+import { set } from 'mongoose';
+import { useState } from 'react';
 
 const links = [
   { linkname: 'Products', path: '/products' },
@@ -37,6 +39,7 @@ const NavLink = ({ path, children }) => (
 const Navbar = () => {
   const { isOpen, onClose, onOpen } = useDisclosure();
   const { colorMode, toggleColorMode } = useColorMode();
+  const { isHovering, setIsHovering } = useState(false);
   return (
     <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
       <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
@@ -47,9 +50,15 @@ const Navbar = () => {
           onClick={isOpen ? onClose : onOpen}
         />
         <HStack>
-          <Link as={ReactLink} to='/'>
+          <Link
+            as={ReactLink}
+            to='/'
+            style={{ textDecoration: 'none' }}
+            onMouseEnter={() => setIsHovering(true)}
+            onMouseLeave={() => setIsHovering(false)}
+          >
             <Flex alignItems={'center'}>
-              <Icon as={GiTechnoHeart} h={6} w={6} color='orange.400' />
+              <Icon as={GiTechnoHeart} h={6} w={6} color={isHovering ? 'cyan.400' : 'orange.400'} />
               <Text fontWeight='extrabold'> Tech Lines </Text>
             </Flex>
           </Link>
